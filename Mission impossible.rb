99c39ed3d5cca968f3deafa4dc4580f3a73f55ef
takis170 <gturner259f@gmail.com>
2026-04-07 10:59:19 -0700

    Update Mission impossible.rb

--- Mission impossible.rb
+++ Mission impossible.rb
@@ -1,15 +1,21 @@
 use_bpm 176
 #mission impossible
 
-use_synth :piano
-#measure 1
-play :E
-sleep 1.5
-play :E
-play :E
-sleep 1.5
-play :G
-sleep 1
-play :A
-sleep 1
-#measure 2
+
+define :Piano_melody do |x, y|
+  use_synth :piano
+  #measure 1
+  play :E
+  sleep 1.5
+  play :E
+  play :E
+  sleep 1.5
+  play x
+  sleep 1
+  play y
+  sleep 1
+end
+14.times do
+  Piano_melody :G, :A
+  Piano_melody :D, :Ds
+end
