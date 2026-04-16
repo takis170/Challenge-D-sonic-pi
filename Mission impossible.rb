e47bc0a254334cf95c7544d143ff917b3c09a14c
takis170 <gturner259f@gmail.com>
2026-04-16 11:22:36 -0700

    Update Mission impossible.rb

--- Mission impossible.rb
+++ Mission impossible.rb
@@ -2,25 +2,11 @@ use_bpm 170
 
 Piccolo = "C:/Users/george_turner/Desktop/Piccolo.wav"
 Guitar = "C:/Users/george_turner/Desktop/GuitarMission.wav"
-
-sample Piccolo, release: 1
-sleep 12
-sample Guitar
-sleep 18
-#mission impossible
-# Mission: Impossible Piccolo Riff
-
-#Main melody
-
-define :p do |note|
-  play note, amp:1
-end
-
-live_loop :testing do
+define :testing do
   use_synth :piano
   play :B5, sustain: 1
   sleep 0.5
-  play :G4, sustain: 1 
+  play :G4, sustain: 1
   sleep 0.5
   play :D5, sustain: 10
   sleep 3.6
@@ -40,4 +26,38 @@ live_loop :testing do
   sleep 0.5
   play :C5
   sleep 4
+  # measure 8
 end
+define :new measure do
+  use_bpm 170
+use_synth :piano
+play :G
+sleep 1.5
+play :G,sustain: 10
+sleep 1
+play :B
+sleep 1
+play :C6
+
+sleep 1
+
+play :G
+sleep 1.5
+play :G,sustain: 10
+end
+
+
+
+sample Piccolo, release: 1
+sleep 12
+sample Guitar
+sleep 18
+#mission impossible
+# Mission: Impossible Piccolo Riff
+
+#Main melody
+
+define :p do |note|
+  play note, amp:1
+end
+testing
