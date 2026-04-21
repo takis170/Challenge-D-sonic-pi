d74263f663869646a84a31dfad8e759d9b4c74e1
takis170 <gturner259f@gmail.com>
2026-04-20 21:35:02 -0700

    Update Mission impossible.rb

--- Mission impossible.rb
+++ Mission impossible.rb
@@ -4,7 +4,7 @@ Piccolo = "C:/Users/gturn/Desktop/2nd CS SONG/Piccolo.wav"
 
 Guitar = "C:/Users/gturn/Desktop/2nd CS SONG/bassMissionImpossible.wav"
 
-define :testing do
+define :bass_clef do
   use_synth :piano
   play :B5, sustain: 1
   sleep 0.5
@@ -27,7 +27,7 @@ define :testing do
   play :B4
   sleep 0.5
   play :C5
-  sleep 3.5
+  sleep 4
   # measure 9
   play :B4
   sleep 1.5
@@ -41,12 +41,5 @@ sample Piccolo, release: 0.5
 sleep 12
 sample Guitar
 sleep 18
-#mission impossible
-# Mission: Impossible Piccolo Riff
-
+bass_clef
 #Main melody
-
-define :p do |note|
-  play note, amp:1
-end
-testing
